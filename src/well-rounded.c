9faf0fce48857e440a639c8f1e94ca98d494375b
David Morgan <dmorgan81@gmail.com>
2016-04-14 09:58:43 -0400

    Initial commit

---
+++ src/well-rounded.c
@@ -0,0 +1,28 @@
+#include <pebble.h>
+
+static Window *s_window;
+
+static void window_load(Window *window) {
+}
+
+static void window_unload(Window *window) {
+}
+
+static void init(void) {
+  s_window = window_create();
+  window_set_window_handlers(s_window, (WindowHandlers) {
+    .load = window_load,
+    .unload = window_unload,
+  });
+  window_stack_push(s_window, true);
+}
+
+static void deinit(void) {
+  window_destroy(s_window);
+}
+
+int main(void) {
+  init();
+  app_event_loop();
+  deinit();
+}
