71152a3a25cfb77da68dac7e88ad4b75af9a55e9
David Morgan <dmorgan81@gmail.com>
2016-04-14 12:16:43 -0400

    Radial layer
    Will be used for battery and health

---
+++ src/layers/radial_layer.c
@@ -0,0 +1,54 @@
+#include "../logging.h"
+#include "radial_layer.h"
+
+typedef struct {
+    int32_t max;
+    int32_t value;
+    uint16_t inset;
+} Internal;
+
+void radial_layer_update_proc(Layer *layer, GContext *ctx) {
+    logd("%s", __func__);
+    GRect bounds = layer_get_bounds(layer);
+    Internal *internal = (Internal *) layer_get_data(layer);
+
+    int32_t angle = TRIG_MAX_ANGLE * internal->value / internal->max;
+    graphics_context_set_fill_color(ctx, GColorWhite);
+    graphics_fill_radial(ctx, bounds, GOvalScaleModeFitCircle, internal->inset, 0, angle);
+}
+
+RadialLayer *radial_layer_create(GRect frame) {
+    logd("%s", __func__);
+    RadialLayer *this = layer_create_with_data(frame, sizeof(Internal));
+    layer_set_update_proc(this, radial_layer_update_proc);
+    Internal *internal = (Internal *) layer_get_data(this);
+
+    internal->max = 0;
+    internal->value = 0;
+    internal->inset = 0;
+
+    return this;
+}
+
+void radial_layer_destroy(RadialLayer *this) {
+    logd("%s", __func__);
+    layer_destroy(this);
+}
+
+void radial_layer_set_value(RadialLayer *this, int32_t value) {
+    logd("%s", __func__);
+    ((Internal *) layer_get_data(this))->value = value;
+    layer_mark_dirty(this);
+}
+
+void radial_layer_set_max(RadialLayer *this, int32_t max) {
+    logd("%s", __func__);
+    ((Internal *) layer_get_data(this))->max = max;
+    layer_mark_dirty(this);
+}
+
+void radial_layer_set_inset(RadialLayer *this, uint16_t inset) {
+    logd("%s", __func__);
+    ((Internal *) layer_get_data(this))->inset = inset;
+    layer_mark_dirty(this);
+}
---
+++ src/layers/radial_layer.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <pebble.h>
+
+typedef Layer RadialLayer;
+
+RadialLayer *radial_layer_create(GRect frame);
+void radial_layer_destroy(RadialLayer *this);
+void radial_layer_set_value(RadialLayer *this, int32_t value);
+void radial_layer_set_max(RadialLayer *this, int32_t max);
+void radial_layer_set_inset(RadialLayer *this, uint16_t inset);
