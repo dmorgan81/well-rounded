20473ac2cba3dd8d55c3db3d5dc6d64df925a253
David Morgan <dmorgan81@gmail.com>
2016-04-15 13:19:17 -0400

    Split out aplite config

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,30 @@
 var Clay = require('clay');
 var config = require('config.json');
-var clay = new Clay(config);
+var apliteConfig = require('config-aplite.json');
+var clay = new Clay(config, null, { autoHandleEvents: false });
+
+Pebble.addEventListener('showConfiguration', function(e) {
+    var platform = clay.meta.activeWatchInfo.platform || 'aplite';
+    if (platform === 'aplite') {
+        clay.config = apliteConfig;
+    }
+
+    Pebble.openURL(clay.generateUrl());
+});
+
+Pebble.addEventListener('webviewclosed', function(e) {
+      if (e && !e.response) {
+        return;
+    }
+
+    // Get the keys and values from each config item
+    var dict = clay.getSettings(e.response);
+
+    // Send settings values to watch side
+    Pebble.sendAppMessage(dict, function(e) {
+        console.log('Sent config data to Pebble');
+    }, function(e) {
+        console.log('Failed to send config data!');
+        console.log(JSON.stringify(e));
+    });
+});
